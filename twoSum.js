836d9f8d08e8af30241fd4ef1c73aa85e84bdf47
Scott Techau <scotttechau@gmail.com>
2018-11-19 11:15:44 -0800

    Flush out twoSum function to pass all tests.

--- twoSum.js
+++ twoSum.js
@@ -1,7 +1,18 @@
 function twoSum(array, target) {
-  if (!array.length) return [];
-
   const pastNums = {};
+
+  for (let i = 0; i < array.length; i++) {
+    const num = array[i];
+    const compliment = target - num;
+    const existingIdx = pastNums[compliment];
+    if (existingIdx !== undefined) {
+      return [existingIdx, i];
+    } else {
+      pastNums[num] = i;
+    }
+  }
+
+  return [];
 }
 
 module.exports = twoSum;
