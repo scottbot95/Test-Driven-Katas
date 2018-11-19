54a4718ddda45264f1440864a58183b4a741f7b3
Scott Techau <scotttechau@gmail.com>
2018-11-19 10:39:34 -0800

    Add in fullstack provided solution to verify tests

--- wrap.js
+++ wrap.js
@@ -6,4 +6,27 @@ function wrap(string, colNum) {
   return string.replace(regexp, '$1\n').trim();
 }
 
+function wrap2(line, maxLength) {
+  if (line.length <= maxLength) {
+    return line;
+  }
+
+  const indexOfBlank = line.lastIndexOf(' ', maxLength);
+  let split;
+  let offset;
+  if (indexOfBlank > -1) {
+    split = indexOfBlank;
+    offset = 1;
+  } else {
+    split = maxLength;
+    offset = 0;
+  }
+
+  return (
+    line.substring(0, split) +
+    '\n' +
+    wrap(line.substring(split + offset), maxLength)
+  );
+}
+
 module.exports = wrap;
